4800114ef5d4ad6cc4def0066f9f524fdd995195
Cleo <126628086+Bamber22@users.noreply.github.com>
2023-04-05 20:00:33 -0400

    Wow
    PONG IS HAPPENING

--- My project/Assets/Scripts/Player.cs
+++ My project/Assets/Scripts/Player.cs
@@ -7,19 +7,27 @@ public class Player : MonoBehaviour
 
     public string playerName;
     private ulong highestpop;
-    private GameplayManager gameManager
+    private GameplayManager gameManager;
     public TMP_Text nameText;
     public TMP_Text yearText;
     public TMP_Text highestPopText;
     // Start is called before the first frame update
     void Start()
     {
-        
+        nameText.text = "Ruler: " + playerName;
+
+        gameManager = GetComponent<GameplayManager>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        yearText.text = "Year: " + gameManager.year;
+        if (gameManager.population > highestpop)
+        {
+            highestpop = gameManager.population;
+            highestPopText.text = "Highest Population: " + highestpop;
+
+        }
     }
 }
