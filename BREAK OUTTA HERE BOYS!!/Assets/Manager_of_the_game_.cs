8f5375009adcf0024fbe04c00e7d3698ce06a8a7
CELO <126628086+Bamber22@users.noreply.github.com>
2023-05-12 19:55:20 -0400

    Yep Ok
    FIx stuff next time

--- BREAK OUTTA HERE BOYS!!/Assets/Manager_of_the_game_.cs
+++ BREAK OUTTA HERE BOYS!!/Assets/Manager_of_the_game_.cs
@@ -6,11 +6,15 @@ public class Manager_of_the_game_ : MonoBehaviour
 {
     public GameObject StupidRatPrefab;
     // Start is called before the first frame update
+    public float xSpace;
+    public float xOffset;
     void Start()
     {
         for (int x = 0; x < 10;x++)
         {
-            Instantiate(StupidRatPrefab, new Vector2(x, 3), Quaternion.identity);
+            Instantiate(StupidRatPrefab, new Vector2(x * xSpace + xOffset, 3), Quaternion.identity);
+            Instantiate(StupidRatPrefab, new Vector2(x * xSpace + xOffset, 3.75f), Quaternion.identity);
+            Instantiate(StupidRatPrefab, new Vector2(x * xSpace + xOffset, 4.5f), Quaternion.identity);
         }
     }
 
