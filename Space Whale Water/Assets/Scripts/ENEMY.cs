a17ba1352365542f331c8828ad30a808fab2e20f
Madelyn <126628086+Bamber22@users.noreply.github.com>
2023-05-31 19:47:20 -0400

    I'm dead inside
    Step 50

--- Space Whale Water/Assets/Scripts/ENEMY.cs
+++ Space Whale Water/Assets/Scripts/ENEMY.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,8 +10,32 @@ public class ENEMY : MonoBehaviour
     // Start is called before the first frame update
     public float xSpace;
     public float xOffset;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        animator.SetTrigger("death");
+        Destroy(gameObject, 1f);
+        Destroy(collision.gameObject);
+    }
+    public float speed;
+    private Vector2 direction;
     void Start()
     {
         animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        transform.Translate(direction * Time.deltaTime * speed);
+
+        if (transform.position.x > 8f)
+        {
+            direction = Vector2.left;
+        }
+
+       if (transform.position.x < -8f)
+        {
+            direction = Vector2.right;
+        }
     }
 }
--- Space Whale Water/Assets/Scripts/Player.cs
+++ Space Whale Water/Assets/Scripts/Player.cs
@@ -3,14 +3,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class bullrt : MonoBehaviour
+public class Player : MonoBehaviour
 {
     public GameObject bulletPrefab;
-    public float speed;
     // Start is called before the first frame update
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
