cc188c388cbfa1e65570cec1a3ab89dccfb75017
Bernice <126628086+Bamber22@users.noreply.github.com>
2023-06-13 19:52:46 -0400

    Yayy
    Fixed

--- Space Whale Water/Assets/Scripts/ENEMY.cs
+++ Space Whale Water/Assets/Scripts/ENEMY.cs
@@ -7,30 +7,32 @@ public class ENEMY : MonoBehaviour
 {
     private Animator animator;
     public float speed;
-    private Vector2 direction;
+    static private Vector2 direction;
 
+   
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        direction = Vector2.right;
+    }
     private void OnCollisionEnter2D(Collision2D collision)
     {
         animator.SetTrigger("death");
         Destroy(gameObject, 1f);
         Destroy(collision.gameObject);
     }
-    void Start()
-    {
-        animator = GetComponent<Animator>();
-    }
-
     void Update()
     {
         transform.Translate(direction * Time.deltaTime * speed);
 
-        if (transform.position.x > 9f)
+        if (transform.position.x > 10f)
         {
-            direction = Vector2.left;
+            direction = Vector2.left; 
             MoveDown();
         }
 
-       if (transform.position.x < -9f)
+       if (transform.position.x < -10f)
         {
             direction = Vector2.right;
             MoveDown();
