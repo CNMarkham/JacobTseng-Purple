55c4790c5f1bb240c5266d6df53a6f73728d3feb
CELO <126628086+Bamber22@users.noreply.github.com>
2023-06-28 19:48:12 -0400

    AYYYYY
    PAC-MAN

--- Meteors/Assets/Gamer.cs
+++ Meteors/Assets/Gamer.cs
@@ -5,19 +5,22 @@ using UnityEngine;
 public class Gamer : MonoBehaviour
 {
     public GameObject rockPrefab;
+    public Transform[] spawnPoints;
     // Start is called before the first frame update
     void Start()
     {
-        for (int i = 0; i < 4; i++)
-        {
-            Vector3 randomPosition = new Vector3(Random.Range(-8, 8), Random.Range(-8, 8), 0f);
-            Instantiate(rockPrefab, randomPosition, Quaternion.identity);
-        }
+        InvokeRepeating("SpawnRocks", 0f, 10f);
     }
-
-    // Update is called once per frame
     void Update()
     {
 
+    }
+    private void SpawnRocks()
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            Vector3 randomPosition = spawnPoints[Random.Range(0, 3)].position;
+            Instantiate(rockPrefab, randomPosition, Quaternion.identity);
+        }
     }
 }
