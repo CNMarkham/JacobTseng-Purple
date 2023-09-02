50d41948dacf56bcc9dfbd622b0deaa800d14278
Cleo <markhamonca@codeninjas.com>
2023-09-02 14:57:33 -0400

    hooray
    step 69

---
+++ Packing-Man Amazon Prime/Assets/PREFAV/Movement.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float speed;
+    public Vector2 initialDirection;
+    public LayerMask obstacleLayer;
+
+    protected Rigidbody2D rb;
+    protected Vector2 direction;
+    protected Vector2 nextDirection;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private bool Occupied(Vector2 newDirection)
+    {
+        RaycastHit2D hit = Physics2D.BoxCast(transform.position, Vector2.one * 0.75f, 0f, newDirection, 1.5f, obstacleLayer);
+        return hit.collider != null;
+    }
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        Vector2 position = rb.position;
+        Vector2 translation = direction * speed * Time.fixedDeltaTime;
+
+        rb.MovePosition(position + translation);
+    }
+
+    protected void SetDirection(Vector2 newDirection)
+    {
+        if (!Occupied(newDirection))
+        {
+            direction = newDirection;
+            nextDirection = Vector2.zero;
+        }
+        else
+        {
+            nextDirection = newDirection;
+        }
+    }
+}
---
+++ Packing-Man Amazon Prime/Assets/PREFAV/pacman.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class pacman : Movement
+{
+    protected override void ChildUpdate()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("vertical");
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (nextDirection != Vector2.zero)
+        {
+            SetDirection(nextDirection);
+        }
+
+        ChildUpdate();
+    }
+}
+
