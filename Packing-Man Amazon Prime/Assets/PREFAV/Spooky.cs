c8880b6bb02637c384f76fe6e37f93f8de8dc530
Emma <markhamonca@codeninjas.com>
2023-10-04 19:55:14 -0400

    uyhwsiusdhiuiuhd
    105

--- Packing-Man Amazon Prime/Assets/PREFAV/Spooky.cs
+++ Packing-Man Amazon Prime/Assets/PREFAV/Spooky.cs
@@ -2,8 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SpookyOOOHHHHH: Movement
+public class Spooky : Movement
 {
+    protected override void ChildUpdate()
+    {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        NODE Node = collision.GetComponent<NODE>();
+
+    }
     // Start is called before the first frame update
     void Start()
     {
