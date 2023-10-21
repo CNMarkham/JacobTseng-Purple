d26476bbd78214d80f920a34c711cfeb7e0c9db8
Kelton <markhamonca@codeninjas.com>
2023-10-21 15:58:17 -0400

    tgnijfudhbuhbdsfuhfdshjkjgih
    uyhfdsdsufhijdfsuhfdij

--- Packing-Man Amazon Prime/Assets/PREFAV/Spooky.cs
+++ Packing-Man Amazon Prime/Assets/PREFAV/Spooky.cs
@@ -4,16 +4,36 @@ using UnityEngine;
 
 public class Spooky : Movement
 {
-  public GameObject body;
+    public float homeDuration;
+    public bool frightened;
+    public GameObject body;
+    public GameObject eyes;
+    public GameObject blue;
+    public GameObject white;
     protected override void ChildUpdate()
+    {
+        transform.position = new Vector3(0, 2.5f, -1f);
+        direction = new Vector2(-1, 0);
+        atHome = false;
+        body.SetActive(true);
+        eyes.SetActive(true);
+        blue.SetActive(false);
+        white.SetActive(false);
+        frightened = false;
+    }
+
+    private void LeaveHome()
     {
 
     }
-
     private void Awake()
     {
         body.SetActive(true);
-        
+        eyes.SetActive(true);
+        blue.SetActive(false);
+        white.SetActive(false);
+        frightened = false;
+        Invoke("LeaveHome", homeDuration);
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
