23ef1b0bfad3a957d2e7265b28d22acc2ba03886
Aroosh <markhamonca@codeninjas.com>
2024-01-13 16:01:31 -0500

    sssssssss
    ssssssssssssssssss

--- JT Mario/Assets/Scripts/AHHHFRESHMEAT.cs
+++ JT Mario/Assets/Scripts/AHHHFRESHMEAT.cs
@@ -2,20 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyMove : MonoBehaviour
+public class AHHHFRESHMEAT : MonoBehaviour
 {
-    public float speed = 2f;
-    public Vector2 direction = Vector2.left;
     private Rigidbody2D rb;
-
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
     }
 
-    private void OnBecameVisible()
+    // Update is called once per frame
+    void Update()
     {
-        rb.velocity = direction * speed;
+        Vector3 velocity = rb.velocity;
+        velocity.y = Mathf.Min(velocity.y, 0);
+        rb.velocity = velocity;
     }
 }
