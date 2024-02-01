64cd3d46ba652fbce44ffd2552fa186de373c44b
CNMarkham <markhamonca@codeninjas.com>
2024-01-31 19:50:13 -0500

    wertytrerfgfcdxvghyutrfdvgbh
    wertyuioplkmnbvcdfgt

--- JT Mario/Assets/Animations/Scripts/Animation.cs
+++ JT Mario/Assets/Animations/Scripts/Animation.cs
@@ -1,20 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 public class Animation : MonoBehaviour
 {
-    
+    private RaycastHit2D hit;
+    private float jumpForce;
+    private bool jumping;
+    public Rigidbody2D rb;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        jumping = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (hit.collider != null && Input.GetKeyDown(KeyCode.Space))
+        {
+            Vector3 velocity = rb.velocity;
+            velocity.y = jumpForce;
+            rb.velocity = velocity;
+        }
     }
 
     private void FlipDirection()
@@ -22,6 +31,16 @@ public class Animation : MonoBehaviour
         foreach (SpriteRenderer sprite in GetComponentsInChildren<SpriteRenderer>())
         {
             sprite.flipX = rb.velocity.x < 0;
+        }
+    }
+
+    private void ChangeAnimations()
+    {
+        foreach (Animator animator in GetComponentsInChildren<Animator>())
+        {
+            animator.SetFloat("velocityX", rb.velocity.x);
+            animator.SetFloat("horizantalInput", Input.GetAxis("Horizontal"));
+            animator.SetBool("inAir", hit.collider == null || jumping);
         }
     }
 }
